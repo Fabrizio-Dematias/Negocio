7b3e24f94f389714a5279d5c73640099a4349f35
Fabrizio <fabri10dematias@hotmail.com>
2024-10-22 20:15:31 -0300

    add about and contact page profe!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
+import Contact from './pages/Contact';
+import About from './pages/About';
+  
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/App.css';
@@ -12,6 +14,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
      
 
       </Routes>
