943d5f014c0f1132ed8a434fd31d28f8c1f7099b
Fabrizio <fabri10dematias@hotmail.com>
2024-10-22 20:06:53 -0300

    commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Bienvenido a Mi Negocio</h1>
+      <p>Explora nuestros productos y servicios.</p>
+    </div>
+  );
+};
+
+export default Home;
